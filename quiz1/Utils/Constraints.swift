18ed67025321b68a74bea3d6fe0d79c9bd356bff
hotpot06 <akdemon0720@gmail.com>
2023-10-21 17:15:21 -0700

    commit quiz1

---
+++ quiz1/Utils/Constraints.swift
@@ -0,0 +1,11 @@
+//
+//  Constraints.swift
+//  quiz1
+//
+//  Created by Hotpot Liu on 10/21/23.
+//
+
+import Foundation
+
+let apiKey = "2c839750e5ff814287b33c79b625773f"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
